9a3c8a54ee44ca54b753ba01b3f7755ea811c660
js402 <ertli.alexander@googlemail.com>
2025-04-25 08:44:12 +0200

    Add documentation

--- core/serverops/activitytracker.go
+++ core/serverops/activitytracker.go
@@ -1,7 +1,32 @@
 package serverops
 
+// ActivityTracker defines a hook interface for observing the lifecycle of an operation.
+// It provides three functions:
+//   - A function to report an error result of the operation.
+//   - A function to report any state changes that occurred during the operation.
+//   - A function to signal the end of the operation.
+//
+// This interface is typically used to wrap service calls in order to:
+//   - Record metrics
+//   - Emit logs
+//   - Track side effects or changes to system state
+//   - Report to external systems such as tracing backends or activity streams
+//
+// Callers should invoke Start at the beginning of an operation, passing any relevant arguments.
+// The returned functions must be invoked to properly report the operation’s result and side effects.
+//
+// Example usage:
+//
+//	reportErr, reportChange, end := tracker.Start("Service.Method", arg1, arg2)
+//	defer end()
+//	result, err := realService.Method(...)
+//	if err != nil {
+//	    reportErr(err)
+//	} else {
+//	    reportChange("some-entity-id")
+//	}
 type ActivityTracker interface {
-	Start(operation string, args ...any) (report func(err error), reportChange func(subject string), end func())
+	Start(operation string, args ...any) (reportErr func(err error), reportChange func(subject string), end func())
 }
 
 type NoopTracker struct{}
